88aee6f3e2829f063f09b9f5d1f59e259dcb26b6
Mike Ore <more@mosaiclearning.com>
2021-10-28 10:42:44 -0400

    setting up vue project

---
+++ client/src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <div id="app">
+     <h1>Mike's Reading List</h1>
+    <!-- <hello-world /> -->
+    <book-list />
+   
+  </div>
+</template>
+
+
+
+<script>
+// import HelloWorld from './components/HelloWorld.vue'
+import BookList from './components/BookList.vue'
+
+export default {
+  name: 'App',
+  components: {
+    // HelloWorld,
+    BookList
+  }
+}
+</script>
+
+
+
+<style>
+body{
+  background:darkgrey
+}
+
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #e6e6e6;
+  margin-top: 60px;
+}
+
+</style>
---
+++ client/src/components/BookList.vue
@@ -0,0 +1,15 @@
+<template>
+<div>
+    <ul id="book-list">
+        <li></li>
+    </ul>
+</div>
+    
+</template>
+
+<script>
+export default {
+    name:'BookList'
+    
+}
+</script>
